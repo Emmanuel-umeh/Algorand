b3e00afbf9293a694b1b56928614a4b50d7a7ab4
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 15:45:25 +0100

    --- Auto-Git Commit ---

--- routes/Transactions.js
+++ routes/Transactions.js
@@ -9,45 +9,51 @@ const algosdk = require("algosdk");
  * utility function to wait on a transaction to be confirmed
  * the timeout parameter indicates how many rounds do you wish to check pending transactions for
  */
- const waitForConfirmation = async function (algodclient, txId, timeout) {
-    // Wait until the transaction is confirmed or rejected, or until 'timeout'
-    // number of rounds have passed.
-    //     Args:
-    // txId(str): the transaction to wait for
-    // timeout(int): maximum number of rounds to wait
-    // Returns:
-    // pending transaction information, or throws an error if the transaction
-    // is not confirmed or rejected in the next timeout rounds
-    if (algodclient == null || txId == null || timeout < 0) {
-        throw "Bad arguments.";
-    }
-    let status = (await algodclient.status().do());
-    if (status == undefined) throw new Error("Unable to get node status");
-    let startround = status["last-round"] + 1;   
-    let currentround = startround;
+const waitForConfirmation = async function (algodclient, txId, timeout) {
+  // Wait until the transaction is confirmed or rejected, or until 'timeout'
+  // number of rounds have passed.
+  //     Args:
+  // txId(str): the transaction to wait for
+  // timeout(int): maximum number of rounds to wait
+  // Returns:
+  // pending transaction information, or throws an error if the transaction
+  // is not confirmed or rejected in the next timeout rounds
+  if (algodclient == null || txId == null || timeout < 0) {
+    throw "Bad arguments.";
+  }
+  let status = await algodclient.status().do();
+  if (status == undefined) throw new Error("Unable to get node status");
+  let startround = status["last-round"] + 1;
+  let currentround = startround;
 
-    while (currentround < (startround + timeout)) {
-        let pendingInfo = await algodclient.pendingTransactionInformation(txId).do();      
-        if (pendingInfo != undefined) {
-            if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
-                //Got the completed Transaction
-                return pendingInfo;
-            }
-            else {
-                if (pendingInfo["pool-error"] != null && pendingInfo["pool-error"].length > 0) {
-                    // If there was a pool error, then the transaction has been rejected!
-                    throw new Error("Transaction Rejected" + " pool error" + pendingInfo["pool-error"]);
-                }
-            }
-        } 
-        await algodclient.statusAfterBlock(currentround).do();
-        currentround++;
+  while (currentround < startround + timeout) {
+    let pendingInfo = await algodclient
+      .pendingTransactionInformation(txId)
+      .do();
+    if (pendingInfo != undefined) {
+      if (
+        pendingInfo["confirmed-round"] !== null &&
+        pendingInfo["confirmed-round"] > 0
+      ) {
+        //Got the completed Transaction
+        return pendingInfo;
+      } else {
+        if (
+          pendingInfo["pool-error"] != null &&
+          pendingInfo["pool-error"].length > 0
+        ) {
+          // If there was a pool error, then the transaction has been rejected!
+          throw new Error(
+            "Transaction Rejected" + " pool error" + pendingInfo["pool-error"]
+          );
+        }
+      }
     }
-    throw new Error("Transaction not confirmed after " + timeout + " rounds!");
+    await algodclient.statusAfterBlock(currentround).do();
+    currentround++;
+  }
+  throw new Error("Transaction not confirmed after " + timeout + " rounds!");
 };
-
-
-
 
 router.post("/", async (req, res) => {
   /*
@@ -59,13 +65,10 @@ router.post("/", async (req, res) => {
 
     */
 
-
-    console.log("key , " , process.env.API_KEY)
-
   const algod_server = "https://testnet-algorand.api.purestake.io/ps2";
   const algod_port = "";
   const algod_token = {
-    "X-API-Key": '8LtYbv0XMB6wBXhJ2dJPR6LUDDXyEZTUrrT97Daa'
+    "X-API-Key": "8LtYbv0XMB6wBXhJ2dJPR6LUDDXyEZTUrrT97Daa",
   };
 
   let algodClient = new algosdk.Algodv2(algod_token, algod_server, algod_port);
@@ -79,21 +82,18 @@ router.post("/", async (req, res) => {
   // (async() => {
 
   try {
-
     // check my account balance
 
-    let accountInfo = await algodClient.accountInformation(recoveredAccount.addr).do();
+    let accountInfo = await algodClient
+      .accountInformation(recoveredAccount.addr)
+      .do();
     console.log("Account balance: %d microAlgos", accountInfo.amount);
 
-    if(accountInfo.amount < 1000000){
-        return res.status(400).json("Balance is too low");
-  
+    if (accountInfo.amount < 1000000) {
+      return res.status(400).json("Balance is too low");
     }
 
-    
     // Create the transaction
-
-
 
     let params = await algodClient.getTransactionParams().do();
 
@@ -115,7 +115,7 @@ router.post("/", async (req, res) => {
     // Sign the transaction
     let signedTxn = txn.signTxn(recoveredAccount.sk);
 
-    console.log({signedTxn})
+    console.log({ signedTxn });
     let txId = txn.txID().toString();
     console.log("Signed transaction with txID: %s", txId);
 
@@ -140,8 +140,6 @@ router.post("/", async (req, res) => {
     console.log({ error });
     return res.status(400).json("Transaction failed");
   }
-
- 
 });
 
 // {
